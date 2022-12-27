d3a894e8e20b20e83c238a87537b57710865c152
bug400 <bug400@gmx.de>
2022-12-27 10:15:45 +0100

    Updated LICENSE, prepare PyPI installation

--- setup.py
+++ setup.py
@@ -32,14 +32,14 @@ setup(
         #   3 - Alpha
         #   4 - Beta
         #   5 - Production/Stable
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 4 - Beta',
 
         # Indicate who your project is intended for
         'Intended Audience :: HP Calculator enthusiasts',
-        'Topic :: Software Development :: Tools',
+        'Topic :: Software Development :: Assemblers',
 
         # Pick your license as you wish (should match "license" above)
-        'License :: OSI Approved :: MIT License',
+        'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
 
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
@@ -49,6 +49,8 @@ setup(
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
     ],
 
     # What does your project relate to?
